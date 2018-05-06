765e35c653436fa5ab7f3265c5fc75b540a1669b
Vineet Sah <vineetsah345@gmail.com>
2018-05-06 23:23:52 +0530

    moment add

--- public/js/index.js
+++ public/js/index.js
@@ -8,15 +8,16 @@ socket.on('disconnect', function () {
 })
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message)
+    var formatedTime=moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from}: ${formatedTime} ${message.text}`);
     jQuery('#messages').append(li);
 })
 socket.on('newLocationMessage', function (message) {
+    var formatedTime=moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>');
     var a = jQuery(`<a target='_blank'>My Current Location</a>`);
-    li.text(`${message.from} :`);
+    li.text(`${message.from}: ${formatedTime} `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -31,6 +32,7 @@ jQuery(document).ready(function () {
             from: 'User',
             text: jQuery('#message').val()
         }, function (data) {
+            jQuery('#message').val('')
             console.log('Got It');
         })
     })
@@ -40,12 +42,18 @@ jQuery(document).ready(function () {
         if (!navigator.geolocation)
             return alert('Geo location in not supported by your browser');
 
+            locationDom.attr('disabled', 'disabled').text('Sending location...');
+
         navigator.geolocation.getCurrentPosition(function (postions) {
+            locationDom.removeAttr('disabled').text('Send location');
+
             socket.emit('createLocationMessage', {
                 latitude: postions.coords.latitude,
                 longitude: postions.coords.longitude
             })
         }, function (error) {
+            locationDom.removeAttr('disabled').text('Send location');
+
             return alert('Unable to fetch location');
         })
     })
